0519b59167487487b84cf43703137ddf24ce550a
Barcof <barcof@github.com>
2018-12-28 14:31:22 +0700

    modify table test adding column levelid

--- Tables/dbo.test.sql
+++ Tables/dbo.test.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[test] (
   [id] [int] NULL,
   [username] [varchar](255) NULL,
-  [dept] [varchar](255) NULL
+  [deptid] [int] NULL,
+  [levelid] [int] NULL
 )
 ON [PRIMARY]
 GO
